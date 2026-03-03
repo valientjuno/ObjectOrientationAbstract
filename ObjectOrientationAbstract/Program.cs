c69ab7a3060d17ebf521778176513112a2450fa4
valientjuno <jessedoake91@gmail.com>
2026-03-03 12:21:57 -0700

    Add project files.

---
+++ ObjectOrientationAbstract/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace ObjectOrientationAbstract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+
+            dog.MakeSound (); // Output: Woof!
+            cat.MakeSound (); // Output: Meow!
+        }
+    }
+
+
+    public abstract class Animal : IAnimal
+    {
+        public string? Name { get; set; }
+
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal Sound!");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+
+    public interface IAnimal
+    {
+        string? Name { get; set; }
+        void MakeSound();
+    }
+
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+
+
+}
+
